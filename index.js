8c661353c35181cded03c61cebe2c05447469443
Sebastian_B <basti.boehler@hotmail.de>
2020-09-18 17:38:25 +0200

    updated

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const port = process.env.PORT || 80
+
 const app = express();
 app.use(bodyParser.urlencoded({
     extended: false
@@ -20,6 +22,6 @@ app.get('/contact', function (req, res) {
     res.sendFile(__dirname + '/site/contact.html');
 })
 
-http.createServer(app).listen(8080, () => {
-    console.log('Express server listening on port 8080');
+http.createServer(app).listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
 });

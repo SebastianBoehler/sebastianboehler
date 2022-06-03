b6e8da4069514536ff8b3f86ad0e4f91fbffb517
Sebastian_B <basti.boehler@hotmail.de>
2022-06-03 13:59:10 +0200

    Clarity tracking

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,25 @@ import Layout from '../components/layout'
 import Header from '../components/header'
 
 function MyApp({ Component, pageProps }) {
-  return <Layout>
-    <Header />
-    <Component {...pageProps} />
-  </Layout>
+  return (
+    <div>
+      <script
+        //eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    })(window, document, "clarity", "script", "c6jlfc7vjo");
+          `
+        }}
+      />
+      <Layout>
+        <Header />
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  )
 }
 
 export default MyApp

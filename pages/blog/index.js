25e49960413bfcb34b2d8c05b33f21529806aff7
Sebastian_B <basti.boehler@hotmail.de>
2022-06-07 12:09:57 +0200

    Blog SSR

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,28 +2,59 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../../components/header'
 import styles from '../../styles/Blog.module.css'
+import Link from 'next/link'
 
-export default function Home() {
+export default function Home({ articles }) {
     return (
-        <main className={styles.main}>
-            <div className={styles.grid}>
-                <div className={styles.card}>
-                    <div className={styles.cardHeader}>
-                        <h3>How to code your own Supreme bot</h3>
-                    </div>
-                    <div className={styles.cardContent}>
-                        <p>Some random text to describe the article</p>
-                    </div>
+        <div>
+            <Head>
+                <title>Sebastian Boehler | Blog</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={styles.main}>
+                <div className={styles.grid}>
+                    {articles.map(article => {
+                        return (
+                            <Link href={`/blog/${article['title']}`} key={`key_${article['title']}`}>
+                                <div className={styles.card}>
+                                    <div className={styles.cardHeader}>
+                                        <h3>{article['title']}</h3>
+                                    </div>
+                                    <div className={styles.cardContent}>
+                                        <p>{article['content']}</p>
+                                    </div>
+                                </div>
+                            </Link>
+                        )
+                    })}
                 </div>
-                <div className={styles.card}>
-                    <div className={styles.cardHeader}>
-                        <h3>How to code your own Supreme bot</h3>
-                    </div>
-                    <div className={styles.cardContent}>
-                        <p>Some random text to describe the article</p>
-                    </div>
-                </div>
-            </div>
-        </main>
+            </main>
+        </div>
     )
+}
+
+//server side rendering
+export async function getServerSideProps({ req, res }) {
+
+    let articles = [
+        {
+            title: 'How to code your own Supreme bot',
+            content: 'Some random text to describe the article',
+        },
+        {
+            title: 'How to code your own Supreme bot',
+            content: 'Some random text to describe the article',
+        },
+        {
+            title: 'How to code your own Supreme bot',
+            content: 'Some random text to describe the article',
+        },
+    ]
+
+
+    return {
+        props: {
+            articles
+        }
+    }
 }

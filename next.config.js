7f1eb4c5015bf4eae82082237d536871c5e803b1
Sebastian_B <basti.boehler@hotmail.de>
2022-09-10 18:51:18 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    MYSQL_HOST: '139.59.156.50',
-    MYSQL_PASSWORD: '@qJZuP^C3s1%',
-    MYSQL_USER: 'extern',
-    PORT: 3000
   },
   api: {
     responseLimit: false,

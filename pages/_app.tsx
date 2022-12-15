7972f16e4253cf3b63d3e1b4c498b3e013cc2ddf
Sebastian Boehler <basti.boehler@hotmail.de>
2022-12-15 20:34:49 +0700

    App wide layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,45 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ConfigProvider, Layout, theme, notification } from 'antd'
+import Menu from '../components/menu'
+import Header from '../components/header'
+import Footer from '../components/footer'
+import useWindowDimensions from '../hooks/windowDimensions'
+
+const { Content, Sider } = Layout
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { height, width } = useWindowDimensions();
+
+  return <ConfigProvider
+    theme={{
+      algorithm: theme.defaultAlgorithm,
+    }}
+  >
+    <Layout style={{ height: '100%' }}>
+      <Header />
+      <Layout>
+        {width > 768 ? <Sider
+          breakpoint="md"
+          collapsedWidth="0"
+          onBreakpoint={(broken) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+          style={{ backgroundColor: 'white' }}
+        >
+          <Menu type='vertical' />
+        </Sider>
+          :
+          <Menu type='horizontal' />
+        }
+        <Content style={{ padding: '30px' }}>
+          <Component {...pageProps} />
+        </Content>
+      </Layout>
+      <Footer />
+    </Layout>
+  </ConfigProvider>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,9 @@
 import { ConfigProvider, Layout, theme, notification } from 'antd'
-import Menu from '../components/menu'
-import Header from '../components/header'
 import Main from '../components/main'
-import Footer from '../components/footer'
-import { useEffect } from 'react'
-import useWindowDimensions from '../hooks/windowDimensions'
-
-const { Content, Sider } = Layout
 
 export default function Home() {
-  const { height, width } = useWindowDimensions();
 
   return (
-    <ConfigProvider
-      theme={{
-        algorithm: theme.defaultAlgorithm,
-      }}
-    >
-      <Layout style={{ height: '100%' }}>
-        <Header />
-        <Layout>
-          {width > 768 ? <Sider
-            breakpoint="md"
-            collapsedWidth="0"
-            onBreakpoint={(broken) => {
-              console.log(broken);
-            }}
-            onCollapse={(collapsed, type) => {
-              console.log(collapsed, type);
-            }}
-            style={{ backgroundColor: 'white' }}
-          >
-            <Menu type='vertical' />
-          </Sider>
-            :
-            <Menu type='horizontal' />
-          }
-          <Content style={{ padding: '30px' }}>
-            <Main />
-          </Content>
-        </Layout>
-        <Footer />
-      </Layout>
-    </ConfigProvider>
+    <Main />
   )
 }

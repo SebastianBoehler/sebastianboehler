7ac622f2050685de3629fc767f3b423910a2a12c
Sebastian Boehler <basti.boehler@hotmail.de>
2023-12-18 10:51:29 +0700

    feat: first implementation of vercel export

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { VercelExport } from "@/components/vercel-export";
+import '@/app/globals.css'
+
+export default function Home() {
+  return (
+    <main>
+      <VercelExport />
+    </main>
+  )
+}

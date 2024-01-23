de7fe5cc6cb0f54845e90c7c141b84098da1681b
Sebastian Boehler <basti.boehler@hotmail.de>
2024-01-23 14:20:15 +0700

    feat: timeline

---
+++ src/app/timeline/page.tsx
@@ -0,0 +1,10 @@
+import { timeline as Timeline } from '@/components/timeline'
+import '@/app/globals.css'
+
+export default function Page() {
+  return (
+    <main>
+      <Timeline />
+    </main>
+  )
+}
